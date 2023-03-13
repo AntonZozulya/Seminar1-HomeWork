2a2d358ba391c30629b4b2cb6c2e6d7808188465
AntonZozulya <antonzo93@mail.ru>
2023-03-14 02:58:21 +0300

    Добавлена задача 1

---
+++ Seminar_1_Tasks/Program.cs
@@ -0,0 +1,18 @@
+﻿
+//Task 1
+
+
+// int n = int.Parse(Console.ReadLine()!);  // 1 способ
+
+    // Console.Clear(); → Для очищения терминала
+    // Console.Write("Введите число "); 
+    // int n = int.Parse(Console.ReadLine()!);
+    // Console.WriteLine(n * n);
+
+// int n = Convert.ToInt32(Console.ReadLine()!); // 2 способ
+
+    // Console.Clear();
+    // Console.WriteLine("Напишете программу которая на вход принемает число и выдает его квадрат число умноженное на само себя");
+    // Console.Write("Введите число "); 
+    // int n = Convert.ToInt32(Console.ReadLine()!);
+    // Console.WriteLine(n * n);
