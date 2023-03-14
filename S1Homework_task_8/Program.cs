d59e25a9feaa499f688aa9f829b34f07b8926a27
AntonZozulya <antonzo93@mail.ru>
2023-03-14 05:58:06 +0300

    S1Homework_task_8

---
+++ S1Homework_task_8/Program.cs
@@ -0,0 +1,9 @@
+﻿//S1Homework task 8 - 
+
+Console.Clear();
+Console.WriteLine("ЗАДАЧА: Напишете программу которая на вход принимает число N, а на выходе показывает все четные числа от 1 до N \nИТОГ: Программа работает корректно");
+Console.Write("Введите число ");
+int a = Convert.ToInt32(Console.ReadLine()!);
+for (int i = 0; i <= a; i += 2)
+    Console.Write($"{i} ");
+Console.WriteLine();
