fb2449f3c4e0dcadd658a087499294e61469ba18
AntonZozulya <antonzo93@mail.ru>
2023-03-14 05:32:55 +0300

    S1Homework_task_6

---
+++ S1Homework_task_6/Program.cs
@@ -0,0 +1,11 @@
+﻿//S1Homework task 6 - 
+
+Console.Clear();
+Console.WriteLine("ЗАДАЧА: Напишете программу которая принемает число на вход и выдает является ли число четным - делится ли оно на 2 без остатка или нет \nИТОГ: Программа работает корректно");
+
+int a = Convert.ToInt32(Console.ReadLine()!);
+
+if(a%2 == 0)
+    Console.WriteLine("Четное");
+else
+    Console.WriteLine("Нечетное");
