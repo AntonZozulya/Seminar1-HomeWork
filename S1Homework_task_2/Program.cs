203e11c0722676b7127fe1a3a429122994528b5d
AntonZozulya <antonzo93@mail.ru>
2023-03-14 04:42:36 +0300

    S1Homework_task_2

---
+++ S1Homework_task_2/Program.cs
@@ -0,0 +1,17 @@
+﻿//S1Homework task 2 - 
+
+Console.Clear();
+Console.WriteLine("ЗАДАЧА: Напишете программу которая на вход принимает 2 числа и выдает какое число больше, а какое меньше \nИТОГ: Программа работает корректно");
+
+Console.Write("Введите первое число: "); 
+int a = Convert.ToInt32(Console.ReadLine()!);
+
+Console.Write("Введите второе число: "); 
+int b = Convert.ToInt32(Console.ReadLine()!);
+
+if(a > b)
+    Console.WriteLine(a);
+else if(b > a)
+    Console.WriteLine(b);
+else
+    Console.WriteLine("числа равны");
